f00bfab5f08623afad70badc1bca0b0fe7fd88c1
Abhishek Satapathy <abhishek_satapathy@outlook.com>
2018-10-06 18:01:11 +0530

    Iteration one content changes

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ var eventDetails = [
 		eventLocation: "MBA Amphitheater",
 		eventTime: "5 pm",
 		eventDate: "MONDAY,1,JANUARY",
-		eventDesc: 'Just-A-Minute (or JAM) is an all round-fun event that is all about the control of the mind over the mouth. Can you make it through sixty seconds of non-stop talking without hesitation, repetition, or deviation? Or will the sheer pressure make you crumble and have your competitors pounce on you in an instant?',
+		eventDesc: "Your pitch, your win. The stage is set, it awaits the ring of your inspiration and the voices of unique minds that aim to reach out and start out on their own. Presente Vous is the ultimate place to team up and get your start up the backing it needs. Don't fret, we also have the help you might need. Mentors to guide you through the process and shape up your ideas to perfection. Some areas of prime importance will be Technology- advances in sustainability, artificial intelligence and virtual reality to name a few, as well as Finances, Education, Health and Wellness. ",
 		eventImgSrc:"/images/samplePoster.jpg"
 	},
 	{
@@ -68,7 +68,7 @@ var eventDetails = [
 		eventLocation: "MBA Amphitheater",
 		eventTime: "5 pm",
 		eventDate: "MONDAY,1,JANUARY",
-		eventDesc: 'This event encompasses the anatomy and physiology of selected body systems, this year limited to nervous and endocrine systems and sense organs.',
+		eventDesc: "If all the world's a stage then the world is coming to VIT Chennai. A world full of entrepreneurs and exciting ventures. There's a change in the winds, so put your sails high and let the journey begin. This StartUp Expo is the place you want to be if you want some company along. They'll be here to showcase their fruits of labour and love, we'll be here to witness such beauty in real time.",
 		eventImgSrc:"/images/samplePoster.jpg"
 	},
 	{
@@ -76,7 +76,7 @@ var eventDetails = [
 		eventLocation: "MBA Amphitheater",
 		eventTime: "5 pm",
 		eventDate: "MONDAY,1,JANUARY",
-		eventDesc: 'Competitors may construct a self-propelled air-levitated vehicle with up to two battery-powered motors that turn one propeller each to levitate and move the vehicle down a track. Competitors must also be tested on their knowledge of classic mechanics and related topics.',
+		eventDesc: "Education is not filling of a pail, but the lighting of the fire”.Unlike the usual round table conferences, we believe in experiencing it the right way. Around the fire, midnight, a cup of coffee and life stories, just to light up your path. Sounds exciting!Campfire conference is all about it.Networking opportunities for the young minds and a chance to hit your mentors mind right! A lifetime of an experience where you can light a fire , they can never put out!",
 		eventImgSrc:"/images/samplePoster.jpg"
 	},
 	{
@@ -84,7 +84,15 @@ var eventDetails = [
 		eventLocation: "MBA Amphitheater",
 		eventTime: "5 pm",
 		eventDate: "MONDAY,1,JANUARY",
-		eventDesc: 'Competitors must design, build and test a mechanical device which uses the energy from a falling mass to transport an egg along a track as quickly as possible and stop as close to the center of a terminal barrier without breaking the egg.',
+		eventDesc: "“Speak so that you can influence the society”. Magnate moulding is an opportunity for the mentors to share the world what it takes to be an Entrepreneur. 90 minutes and you will sparkle with the fire of want to become like them in your eyes. Converting a startup in crises into a proper business plan will let you a jist of their journey. Enough to inspire you to work hard so that your dreams can come true. Come, listen, build and enjoy the feast for the soul.",
+		eventImgSrc:"/images/samplePoster.jpg"
+	},
+	{
+		eventName: "",
+		eventLocation: "",
+		eventTime: "",
+		eventDate: "",
+		eventDesc: "Open your eyes, rise and shine, suit up quirky, quiet or fine. But let us remind, do not leave your cv behind.This edition of the Internship Drive comes with the promise to let you be the best while learning from the best. Theoretical knowledge is available in plenty but application is the real decider, to get yourself a rung up on the ladder. While you may be looking at some like minds, the startups may find in you their right choice. ",
 		eventImgSrc:"/images/samplePoster.jpg"
 	},
 
@@ -125,7 +133,7 @@ app.post("/register", function (req, res) {
 
 // Login and Logout
 app.get("/login", function (req, res) {
-	res.render("login");
+	res.render("front_page");
 });
 
 app.post("/login", passport.authenticate("local", {
