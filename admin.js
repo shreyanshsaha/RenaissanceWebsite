16fd4da85c79faf75a4c7e6ab09bf17ebaee4b07
Priyam <priyamgupta340@gmail.com>
2018-12-08 17:39:41 +0530

    Teanm deletion module not working

--- admin.js
+++ admin.js
@@ -17,7 +17,7 @@ var Team = require("./models/teamModel");
 // ===========
 // Middlewares
 // ===========
-/*
+
 function isAdmin(req, res, next) {
 	if (req.isAuthenticated() && req.user.isAdmin === true)
 		return next();
@@ -25,29 +25,40 @@ function isAdmin(req, res, next) {
 }
 
 router.use(isAdmin);
-*/
+
 // ===========
 // Admin pages
 // ===========
 
 // Main Admin Page
-router.get("/admin", async function (req, res) {
-	var user = await User.find().populate(" teamId");
-	return res.render("admin_page", { user: user });
+router.get("/admin", isAdmin,async function (req, res) {
+	var user = await User.find().populate("teamId");
+	var team = await Team.find().populate("teamMembers");
+
+	var leader = await Team.find({},{teamLeader:1,_id:0}).populate("teamLeader");
+	return res.render("admin_page", { team: team ,user:user,leader:leader,team1:team1});
 });
 
 // Delete any user
-router.get('/admin/delete/:id',async function (req, res) {
+router.get('/admin/delete/:id',isAdmin,async function (req, res) {
 	User.remove({ _id: req.params.id, isAdmin: false }, function (err, deledata) {
 		res.redirect("/admin");
 	});
 });
 //team deletion module by admin
-router.get("/admin", async function (req, res) {
-	var team = await Team.find().populate("teamMembers");
+/*
+router.get("/admin/deleteleader/:id", async function (req, res) {
+	Team.deleteOne({_id: req.params.id}, async function(err, deletedTeam){
+		// Delete its executive summary
+		await Summary.deleteOne({teamId: req.params.id})
+		.catch((err)=>{
+			return res.send(err);
+		});
 
-	return res.render("admin_page", { team: team });
-});
+
+	});
+
+});*/
 
 
 module.exports = router;
